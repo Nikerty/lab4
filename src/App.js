eb7c9e488561e4904a9b2c8cdd65605765823980
Nikerty <nikertylitik@gmail.com>
2023-12-11 23:11:54 +0300

    chore: another change

--- src/App.js
+++ src/App.js
@@ -3,13 +3,22 @@ import './App.css';
 
 function App() {
   const greeting = "новое изменение";
+
+  const vers = "Its 1.0.1 version"
+
+  const onClick = () => {
+    console.log("hello world")
+  }
+
+
   return (
     <div className="App">
      <header className="App-header">
          {greeting}
       </header>
       <p>helloooo</p>
-       <div>buyyyy</div>
+       <div>{vers}</div>
+       <button onClick={onClick}></button>
     </div>
    
   );
